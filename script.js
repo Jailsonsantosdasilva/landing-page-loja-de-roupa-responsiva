2dafef86bf4f3dc0dbd2f4ff186cb5eeafefddce
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-11-04 01:32:56 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -17,3 +17,56 @@ menuToggle.addEventListener('click', ()=>{
 function comprar(){
     alert("Produto Comprado")
 }
+
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".main-container .main-box", {
+    ...scrollRevealOption,
+   origin:"right",
+   delay: 600,
+})
+
+ScrollReveal().reveal(".main-container .main-box h2", {
+    ...scrollRevealOption,
+    origin:"right",
+   delay: 700,
+})
+
+ScrollReveal().reveal(".main-container .main-box h3", {
+    ...scrollRevealOption,
+   origin:"right",
+   delay: 800,
+})
+
+ScrollReveal().reveal(".main-container .main-box p", {
+    ...scrollRevealOption,
+   origin:"right",
+   delay: 900,
+})
+
+ScrollReveal().reveal(".nav-links li", {
+    ...scrollRevealOption,
+   origin:"top",
+   delay: 400,
+   interval: 300,
+})
+
+ScrollReveal().reveal(".icons i", {
+    ...scrollRevealOption,
+   origin:"top",
+   delay: 1000,
+   interval: 500,
+})
+
+ScrollReveal().reveal(".header-box .logo", {
+    ...scrollRevealOption,
+   origin:"left",
+   delay: 800,
+})
+
+
+ AOS.init();
